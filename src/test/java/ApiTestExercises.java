a1a502616437d92642f2ef7642be7c57fa5df500
gunay.abbasova <gunay.abbasova@cybernet.az>
2025-08-10 19:12:58 +0400

    tapsiriq 2

--- src/test/java/ApiTestExercises.java
+++ src/test/java/ApiTestExercises.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.equalTo;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ public class ApiTestExercises {
   @BeforeAll
   public static void setup() {
     RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    RestAssured.baseURI = "https://jsonplaceholder.typicode.com/posts";
 
   }
 
@@ -31,9 +33,31 @@ public class ApiTestExercises {
        .body("userId", equalTo(2))
         .body("title", equalTo("abhb"));
 
-
-
-
   }
 
+  @Test
+  public void postRequest() {
+
+    String url="https://jsonplaceholder.typicode.com/posts";
+
+    JSONObject requestBody = new JSONObject();
+
+    requestBody.put("title", "Yeni Post");
+    requestBody.put("body", "Bu POST sorğusudur");
+    requestBody.put("userId", 9);
+    System.out.println(requestBody.toString());
+
+    Response response=given().contentType(ContentType.JSON)
+        .when().body(requestBody.toString())
+        .post(url);
+
+    response.prettyPrint();
+
+    //assertion
+    response.then().assertThat().statusCode(201)
+        .contentType(ContentType.JSON)
+        .body("title",equalTo("Yeni Post"))
+        .body("userId",equalTo(9));
+
+}
 }
